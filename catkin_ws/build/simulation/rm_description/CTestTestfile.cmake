e77de62daa6aa541b12fb7182aea66e66dddfd15
sytasch <selahittinsaytas@gmail.com>
2019-12-01 15:30:58 -0800

    URDF xacro macros

---
+++ catkin_ws/build/simulation/rm_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sytasch/ROS/robotic_manipulator/catkin_ws/src/simulation/rm_description
+# Build directory: /home/sytasch/ROS/robotic_manipulator/catkin_ws/build/simulation/rm_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
